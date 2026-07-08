3de73b77fddc34b8bd305f581eb7a0a7076232ca
Vladimir <64512727+dirafu@users.noreply.github.com>
2026-07-08 15:37:21 +0400

    '169. Majority Element' naive solution

---
+++ session-01/169. Majority Element/solution.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <map>
+
+class Solution {
+private:
+    typedef std::vector<int>::iterator It;
+public:
+    int majorityElement(std::vector<int>& nums) {
+      std::map<int, int> freq;
+      for (It it = nums.begin(); it != nums.end(); ++it) {
+        if (++freq[*it] > (nums.size() / 2)) {
+          return (*it);
+        }
+      }
+      return (0);
+    }
+};
+
+#include <iostream>
+int main(void)
+{
+  Solution test;
+  std::vector<int> test_vec{2,2,1,1,1,2,2};
+  std::cout << test.majorityElement(test_vec) << std::endl;
+}
