f4e06ea327275f4aa8d5252d37c3be3d258ae067
Vladimir <64512727+dirafu@users.noreply.github.com>
2026-08-18 00:59:58 +0400

    13. Roman to Integer

---
+++ session-03/13. Roman to Integer/solution-naive.cpp
@@ -0,0 +1,50 @@
+//space complexity: O(1)
+//time complexity: O(n)
+
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+  int romanToInt(std::string s) {
+    std::unordered_map<char, int> alphabet = {
+      {'I', 1},
+      {'V', 5},
+      {'X', 10},
+      {'L', 50},
+      {'C', 100},
+      {'D', 500},
+      {'M', 1000},
+    };
+    int result = 0;
+    for (std::string::reverse_iterator r_it = s.rbegin(), prev_it = r_it; r_it != s.rend(); ++r_it){
+      if (alphabet[*r_it] < alphabet[*prev_it]){
+        result -= alphabet[*r_it];
+      }
+      else
+        result += alphabet[*r_it];
+      prev_it = r_it;
+    }
+    return result;
+  }
+};
+
+#include <iostream>
+#include "../../utils.hpp"
+int main(void)
+{
+  Solution test;
+  std::vector<std::pair<std::string, int>> test_cases = {
+    {"III", 3},
+    {"LVIII", 58},
+    {"MCMXCIV", 1994}
+  };
+
+  std::vector<std::pair<std::string, int>>::iterator it;
+  for (it = test_cases.begin(); it != test_cases.end(); ++it){
+    int result;
+    std::cout << it->first << '=';
+    std::cout << (result = test.romanToInt(it->first)) << ':'
+      << ((result == it->second) ? Color::GREEN + "OK" : Color::RED + "KO") << Color::RESET << '\n';
+  }
+}
