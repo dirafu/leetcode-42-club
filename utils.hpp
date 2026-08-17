ff0d4f3c098222b0fbfa9893733912b4cd0343c3
Vladimir <64512727+dirafu@users.noreply.github.com>
2026-08-17 19:05:20 +0400

    utils.hpp

---
+++ utils.hpp
@@ -0,0 +1,25 @@
+#include <ostream>
+#include <vector>
+
+std::ostream& operator<<(std::ostream& lhs, const std::vector<int>& rhs)
+{
+  std::vector<int>::const_iterator it;
+  lhs << '[';
+  for (it = rhs.begin(); it != rhs.end(); ++it){
+    if (it != rhs.begin())
+      lhs << ", ";
+    lhs << *it;
+  }
+  lhs << ']';
+  return lhs;
+}
+
+namespace Color {
+    const std::string RESET   = "\033[0m";
+    const std::string RED     = "\033[31m";
+    const std::string GREEN   = "\033[32m";
+    const std::string YELLOW  = "\033[33m";
+    const std::string BLUE    = "\033[34m";
+    const std::string MAGENTA = "\033[35m";
+    const std::string CYAN    = "\033[36m";
+}
